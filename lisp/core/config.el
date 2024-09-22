11aa829a89b694661402dc858636d077ed686d45
rondDev <contact@rond.cc>
2024-09-22 17:04:16 +0200

    feat: add cape
    fix: add corfu variables

--- lisp/core/config.el
+++ lisp/core/config.el
@@ -33,8 +33,31 @@
 
 (use-package better-defaults)
 
+(use-package cape
+  ;; Bind prefix keymap providing all Cape commands under a mnemonic key.
+  ;; Press C-c p ? to for help.
+  :bind ("C-c p" . cape-prefix-map) ;; Alternative keys: M-p, M-+, ...
+  ;; Alternatively bind Cape commands individually.
+  ;; :bind (("C-c p d" . cape-dabbrev)
+  ;;        ("C-c p h" . cape-history)
+  ;;        ("C-c p f" . cape-file)
+  ;;        ...)
+  :init
+  ;; Add to the global default value of `completion-at-point-functions' which is
+  ;; used by `completion-at-point'.  The order of the functions matters, the
+  ;; first function returning a result wins.  Note that the list of buffer-local
+  ;; completion functions takes precedence over the global list.
+  (add-hook 'completion-at-point-functions #'cape-dabbrev)
+  (add-hook 'completion-at-point-functions #'cape-file)
+  (add-hook 'completion-at-point-functions #'cape-elisp-block))
+;; (add-hook 'completion-at-point-functions #'cape-history)
+;; ...
+
+
 (use-package corfu
   :init
+  (setq corfu-auto t
+        corfu-quit-no-match t)
   (global-corfu-mode))
 
 (use-package consult
