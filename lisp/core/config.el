f3e1806d7c7f81443d6aa23a324524514cf22dab
rondDev <contact@rond.cc>
2024-09-26 20:15:31 +0200

    feat: add dabbrev to corfu

--- lisp/core/config.el
+++ lisp/core/config.el
@@ -69,6 +69,18 @@
 ;;   :ensure (:wait t)
 ;;   :after projectile)
 
+;; Use Dabbrev with Corfu!
+(use-package dabbrev
+  ;; Swap M-/ and C-M-/
+  :bind (("M-/" . dabbrev-completion)
+         ("C-M-/" . dabbrev-expand))
+  :config
+  (add-to-list 'dabbrev-ignored-buffer-regexps "\\` ")
+  ;; Since 29.1, use `dabbrev-ignored-buffer-regexps' on older.
+  (add-to-list 'dabbrev-ignored-buffer-modes 'doc-view-mode)
+  (add-to-list 'dabbrev-ignored-buffer-modes 'pdf-view-mode)
+  (add-to-list 'dabbrev-ignored-buffer-modes 'tags-table-mode))
+
 (use-package dashboard
   :ensure t
   :config
