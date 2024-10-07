8b48662aa030da452a8cd44301f232fdbac9e1f8
rondDev <contact@rond.cc>
2024-10-07 20:52:45 +0200

    feat: try to prevent flashbang

--- early-init.el
+++ early-init.el
@@ -1,1 +1,10 @@
 (setq package-enable-at-startup nil)
+
+(defun prot-emacs-avoid-initial-flash-of-light ()
+  "Avoid flash of light when starting Emacs, if needed.
+New frames are instructed to call `prot-emacs-re-enable-frame-theme'."
+  (setq mode-line-format nil)
+  (set-face-attribute 'default nil :background "#000000" :foreground "#ffffff")
+  (set-face-attribute 'mode-line nil :background "#000000" :foreground "#ffffff" :box 'unspecified))
+
+(prot-emacs-avoid-initial-flash-of-light)
