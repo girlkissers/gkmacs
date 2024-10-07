70cec98e952d9fc618cea70f93e451fedffb4704
rondDev <contact@rond.cc>
2024-10-07 17:55:09 +0200

    feat: add tempel
    fix: move general to keybinds file

--- lisp/core/config.el
+++ lisp/core/config.el
@@ -178,8 +178,6 @@
   (evil-collection-init)
   (evil-collection-ibuffer-setup))
 
-
-
 (use-package evil-goggles)
 
 (use-package evil-nerd-commenter
@@ -190,30 +188,45 @@
   :config
   (global-evil-surround-mode 1))
 
-(use-package general
-  :ensure (:wait t)
-  :config
-  (general-evil-setup)
-  (general-auto-unbind-keys)
+;; Configure Tempel
+(use-package tempel
+  ;; Require trigger prefix before template name when completing.
+  ;; :custom
+  ;; (tempel-trigger-prefix "<")
 
-  (general-define-key
-   :states '(normal visual motion)
-   "L" 'evil-end-of-line
-   "H" 'evil-first-non-blank)
+  :bind (("M-+" . tempel-complete) ;; Alternative tempel-expand
+         ("M-*" . tempel-insert))
 
-  (general-create-definer gk/leader-keys
-    :states '(normal insert visual emacs)
-    :prefix "SPC"
-    :global-prefix "M-SPC")
-  (general-create-definer gk/local-leader-keys
-    :states '(normal insert visual emacs)
-    :prefix ",")
-  (general-create-definer gk/evil-keys
-    :states '(normal insert visual emacs))
-  (general-create-definer gk/evil-m-keys
-    :states '(motion))
-  (general-create-definer gk/evil-v-keys
-    :states '(visual)))
+  :init
+
+  ;; Setup completion at point
+  (defun tempel-setup-capf ()
+    ;; Add the Tempel Capf to `completion-at-point-functions'.
+    ;; `tempel-expand' only triggers on exact matches. Alternatively use
+    ;; `tempel-complete' if you want to see all matches, but then you
+    ;; should also configure `tempel-trigger-prefix', such that Tempel
+    ;; does not trigger too often when you don't expect it. NOTE: We add
+    ;; `tempel-expand' *before* the main programming mode Capf, such
+    ;; that it will be tried first.
+    (setq-local completion-at-point-functions
+                (cons #'tempel-expand
+                      completion-at-point-functions)))
+
+  ;; Optionally make the Tempel templates available to Abbrev,
+  ;; either locally or globally. `expand-abbrev' is bound to C-x '.
+  ;; (add-hook 'prog-mode-hook #'tempel-abbrev-mode)
+  ;; (global-tempel-abbrev-mode)
+  (add-hook 'conf-mode-hook 'tempel-setup-capf)
+  (add-hook 'prog-mode-hook 'tempel-setup-capf)
+  (add-hook 'text-mode-hook 'tempel-setup-capf))
+
+
+
+;; Optional: Add tempel-collection.
+;; The package is young and doesn't have comprehensive coverage.
+(use-package tempel-collection)
+
+
 
 (use-package helpful
   :defer t)
