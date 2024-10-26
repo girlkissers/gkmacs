68b2f16c1046f8fb324a410a063fd69e53f77393
rondDev <contact@rond.cc>
2024-10-26 10:49:22 +0200

    feat: sudo find fine

--- lisp/core/gk-funcs.el
+++ lisp/core/gk-funcs.el
@@ -57,5 +57,11 @@ things you want byte-compiled in them! Like function/macro definitions."
                (setq body `((after! ,next ,@body)))))
             (`(after! (:and ,@package) ,@body))))))
 
+(defun sudo-find-file (file-name)
+  "Like find file, but opens the file as root."
+  (interactive "FSudo Find File: ")
+  (let ((tramp-file-name (concat "/sudo::" (expand-file-name file-name))))
+    (find-file tramp-file-name)))
+
 (provide 'gk-funcs)
 ;;; gk-funcs.el ends here
